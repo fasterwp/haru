9b48cb2aa10ac009c7b014d2c1e2af205153977b
Andrei Chira <andreichira777@gmail.com>
2018-08-22 14:13:13 +0300

    new base
    mobile-first 2.6.0 genesis sample base

--- functions.php
+++ functions.php
@@ -7,138 +7,422 @@
  * @package Genesis Sample
  * @author  StudioPress
  * @license GPL-2.0+
- * @link    http://www.studiopress.com/
+ * @link    https://www.studiopress.com/
  */
 
-//* Start the engine
-include_once( get_template_directory() . '/lib/init.php' );
+// Starts the engine.
+require_once get_template_directory() . '/lib/init.php';
 
-//* Setup Theme
-include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+// Sets up the Theme.
+require_once get_stylesheet_directory() . '/lib/theme-defaults.php';
 
-//* Set Localization (do not remove)
-load_child_theme_textdomain( 'genesis-sample', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'genesis-sample' ) );
+add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
+/**
+ * Sets localization (do not remove).
+ *
+ * @since 1.0.0
+ */
+function genesis_sample_localization_setup() {
 
-//* Add Image upload and Color select to WordPress Theme Customizer
-require_once( get_stylesheet_directory() . '/lib/customize.php' );
-
-//* Include Customizer CSS
-include_once( get_stylesheet_directory() . '/lib/output.php' );
-
-//* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'HARU Theme' );
-define( 'CHILD_THEME_URL', 'https://fasterwp.com/downloads/haru/' );
-define( 'CHILD_THEME_VERSION', '2.2.4' );
-
-//* Enqueue Scripts and Styles
-add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
-function genesis_sample_enqueue_scripts_styles() {
-
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Fira+Sans:300,300i,400,700', array(), CHILD_THEME_VERSION );
-	wp_enqueue_style( 'dashicons' );
-
-	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
-	$output = array(
-		'mainMenu' => __( 'Menu', 'genesis-sample' ),
-		'subMenu'  => __( 'Menu', 'genesis-sample' ),
-	);
-	wp_localize_script( 'genesis-sample-responsive-menu', 'genesisSampleL10n', $output );
+	load_child_theme_textdomain( 'genesis-sample', get_stylesheet_directory() . '/languages' );
 
 }
 
-//* Add HTML5 markup structure
-add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+// Adds helper functions.
+require_once get_stylesheet_directory() . '/lib/helper-functions.php';
 
-//* Add Accessibility support
-add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+// Adds image upload and color select to Customizer.
+require_once get_stylesheet_directory() . '/lib/customize.php';
 
-//* Add viewport meta tag for mobile browsers
-add_theme_support( 'genesis-responsive-viewport' );
+// Includes Customizer CSS.
+require_once get_stylesheet_directory() . '/lib/output.php';
 
-//* Add support for custom header
-add_theme_support( 'custom-header', array(
-	'width'           => 600,
-	'height'          => 160,
-	'header-selector' => '.site-title a',
-	'header-text'     => false,
-	'flex-height'     => true,
-) );
+// Adds WooCommerce support.
+require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php';
 
-//* Add support for custom background
-add_theme_support( 'custom-background' );
+// Adds the required WooCommerce styles and Customizer CSS.
+require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php';
 
-//* Add support for after entry widget
+// Adds the Genesis Connect WooCommerce notice.
+require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php';
+
+// Defines the child theme (do not remove).
+define( 'CHILD_THEME_NAME', 'MASA Theme' );
+define( 'CHILD_THEME_URL', 'https://fasterwp.com/downloads/masa/' );
+define( 'CHILD_THEME_VERSION', '2.6.1' );
+
+add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
+/**
+ * Enqueues scripts and styles.
+ *
+ * @since 1.0.0
+ */
+function genesis_sample_enqueue_scripts_styles() {
+
+	wp_enqueue_style(
+		'google-fonts',
+		'//fonts.googleapis.com/css?family=IBM+Plex+Sans:400,400i,600,700',
+		array(),
+		CHILD_THEME_VERSION
+	);
+	wp_enqueue_style( 'dashicons' );
+	wp_enqueue_style(
+		'line-awesome',
+		'https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css'
+	);
+
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	wp_enqueue_script(
+		'genesis-sample-responsive-menu',
+		get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js",
+		array( 'jquery' ),
+		CHILD_THEME_VERSION,
+		true
+	);
+	wp_localize_script(
+		'genesis-sample-responsive-menu',
+		'genesis_responsive_menu',
+		genesis_sample_responsive_menu_settings()
+	);
+
+	wp_enqueue_script(
+		'genesis-sample',
+		get_stylesheet_directory_uri() . '/js/genesis-sample.js',
+		array( 'jquery' ),
+		CHILD_THEME_VERSION,
+		true
+	);
+
+}
+
+/**
+ * Defines responsive menu settings.
+ *
+ * @since 2.3.0
+ */
+function genesis_sample_responsive_menu_settings() {
+
+	$settings = array(
+		'mainMenu'         => __( 'Menu', 'genesis-sample' ),
+		'menuIconClass'    => 'dashicons-before dashicons-menu',
+		'subMenu'          => __( 'Submenu', 'genesis-sample' ),
+		'subMenuIconClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'      => array(
+			'combine' => array(
+				'.nav-primary',
+			),
+			'others'  => array(),
+		),
+	);
+
+	return $settings;
+
+}
+
+// Sets the content width based on the theme's design and stylesheet.
+if ( ! isset( $content_width ) ) {
+	$content_width = 702; // Pixels.
+}
+
+// Adds support for HTML5 markup structure.
+add_theme_support(
+	'html5', array(
+		'caption',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'search-form',
+	)
+);
+
+// Adds support for accessibility.
+add_theme_support(
+	'genesis-accessibility', array(
+		'404-page',
+		'drop-down-menu',
+		'headings',
+		'rems',
+		'search-form',
+		'skip-links',
+	)
+);
+
+// Adds viewport meta tag for mobile browsers.
+add_theme_support(
+	'genesis-responsive-viewport'
+);
+
+// Adds custom logo in Customizer > Site Identity.
+add_theme_support(
+	'custom-logo', array(
+		'height'      => 120,
+		'width'       => 700,
+		'flex-height' => true,
+		'flex-width'  => true,
+	)
+);
+
+// Renames primary and secondary navigation menus.
+add_theme_support(
+	'genesis-menus', array(
+		'primary'   => __( 'Header Menu', 'genesis-sample' ),
+		'secondary' => __( 'Footer Menu', 'genesis-sample' ),
+	)
+);
+
+// Adds support for after entry widget.
 add_theme_support( 'genesis-after-entry-widget-area' );
 
-//* Add support for 3-column footer widgets
+// Adds support for 3-column footer widgets.
 add_theme_support( 'genesis-footer-widgets', 3 );
 
-//* Add Image Sizes
-add_image_size( 'featured-image', 720, 400, TRUE );
+// Removes header right widget area.
+unregister_sidebar( 'header-right' );
 
-//* Rename primary and secondary navigation menus
-add_theme_support( 'genesis-menus' , array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+// Removes secondary sidebar.
+unregister_sidebar( 'sidebar-alt' );
 
-//* Reposition the secondary navigation menu
+// Removes site layouts.
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-content-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
+
+// Removes output of primary navigation right extras.
+remove_filter( 'genesis_nav_items', 'genesis_nav_right', 10, 2 );
+remove_filter( 'wp_nav_menu_items', 'genesis_nav_right', 10, 2 );
+
+add_action( 'genesis_theme_settings_metaboxes', 'genesis_sample_remove_metaboxes' );
+/**
+ * Removes output of unused admin settings metaboxes.
+ *
+ * @since 2.6.0
+ *
+ * @param string $_genesis_admin_settings The admin screen to remove meta boxes from.
+ */
+function genesis_sample_remove_metaboxes( $_genesis_admin_settings ) {
+
+	remove_meta_box( 'genesis-theme-settings-header', $_genesis_admin_settings, 'main' );
+	remove_meta_box( 'genesis-theme-settings-nav', $_genesis_admin_settings, 'main' );
+
+}
+
+add_filter( 'genesis_customizer_theme_settings_config', 'genesis_sample_remove_customizer_settings' );
+/**
+ * Removes output of header settings in the Customizer.
+ *
+ * @since 2.6.0
+ *
+ * @param array $config Original Customizer items.
+ * @return array Filtered Customizer items.
+ */
+function genesis_sample_remove_customizer_settings( $config ) {
+
+	unset( $config['genesis']['sections']['genesis_header'] );
+	return $config;
+
+}
+
+// Displays custom logo.
+add_action( 'genesis_site_title', 'the_custom_logo', 0 );
+
+// Repositions primary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_header', 'genesis_do_nav', 12 );
+
+// Repositions the secondary navigation menu.
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
+add_action( 'genesis_footer', 'genesis_do_subnav', 10 );
 
-//* Reduce the secondary navigation menu to one level depth
 add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+/**
+ * Reduces secondary navigation menu to one level depth.
+ *
+ * @since 2.2.3
+ *
+ * @param array $args Original menu options.
+ * @return array Menu options with depth set to 1.
+ */
 function genesis_sample_secondary_menu_args( $args ) {
 
-	if ( 'secondary' != $args['theme_location'] ) {
+	if ( 'secondary' !== $args['theme_location'] ) {
 		return $args;
 	}
 
 	$args['depth'] = 1;
-
 	return $args;
 
 }
 
-//* Modify size of the Gravatar in the author box
 add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+/**
+ * Modifies size of the Gravatar in the author box.
+ *
+ * @since 2.2.3
+ *
+ * @param int $size Original icon size.
+ * @return int Modified icon size.
+ */
 function genesis_sample_author_box_gravatar( $size ) {
 
 	return 90;
 
 }
 
-//* Modify size of the Gravatar in the entry comments
 add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+/**
+ * Modifies size of the Gravatar in the entry comments.
+ *
+ * @since 2.2.3
+ *
+ * @param array $args Gravatar settings.
+ * @return array Gravatar settings with modified size.
+ */
 function genesis_sample_comments_gravatar( $args ) {
 
 	$args['avatar_size'] = 60;
-
 	return $args;
 
 }
-//* Register new widget
+// Register front-page widget areas
+for ( $i = 1; $i <= 5; $i++ ) {
+	genesis_register_widget_area(
+		array(
+			'id'          => "front-page-{$i}",
+			'name'        => __( "Front Page {$i}", 'genesis-sample' ),
+			'description' => __( "This is the front page {$i} section.", 'genesis-sample' ),
+		)
+	);
+}
 
-genesis_register_sidebar( array(
-    'id' => 'after-header',
-    'name' => __( 'After Header', 'genesis-sample' ),
-    'description' => __( 'This is the after header before content section.', 'genesis-sample' ),
+// Enable shortcodes in text widgets
+add_filter('widget_text','do_shortcode');
 
-) );
+/**
+ * Add "first" and "last" CSS classes to dynamic sidebar widgets. Also adds numeric index class for each widget (widget-1, widget-2, etc.)
+ */
+ function widget_first_last_classes( $params ) {
 
-/** Add the home featured section */
+		global $my_widget_num; // Global a counter array
+		$this_id = $params[0]['id']; // Get the id for the current sidebar we're processing
+		$arr_registered_widgets = wp_get_sidebars_widgets(); // Get an array of ALL registered widgets
 
-add_action( 'genesis_after_header', 'kimi_after_header' );
+		if( !$my_widget_num ) {// If the counter array doesn't exist, create it
+			$my_widget_num = array();
+		}
 
-function kimi_after_header() {
+		// if( !isset( $arr_registered_widgets[$this_id] ) || !is_array( $arr_registered_widgets[$this_id] ) ) { // Check if the current sidebar has no widgets
+			// return $params; // No widgets in this sidebar... bail early.
+		// }
 
-	if (is_active_sidebar( 'after-header' )) {
+		if( isset( $my_widget_num[$this_id] ) ) { // See if the counter array has an entry for this sidebar
+			$my_widget_num[$this_id] ++;
+		} else { // If not, create it starting with 1
+			$my_widget_num[$this_id] = 1;
+		}
 
-		genesis_widget_area( 'after-header', array(
+		$class = 'class="widget-' . $my_widget_num[$this_id] . ' '; // Add a widget number class for additional styling options
 
- 			'before' => '<div class="after-header widget-area"><div class="wrap"',
+		if( $my_widget_num[$this_id] == 1 ) { // If this is the first widget
+			$class .= 'widget-first ';
+		} elseif( $my_widget_num[$this_id] == count( $arr_registered_widgets[$this_id] ) ) { // If this is the last widget
+			$class .= 'widget-last ';
+		}
 
-			'after' => '</div></div>'
+		// $params[0]['before_widget'] = str_replace( 'class="', $class, $params[0]['before_widget'] ); // Insert our new classes into "before widget"
+		$params[0]['before_widget'] = preg_replace('/class=\"/', "$class", $params[0]['before_widget'], 1); // Insert our new classes into "before widget"
 
-			) );
+		return $params;
 
+	}
+	add_filter( 'dynamic_sidebar_params', 'widget_first_last_classes' );
+
+	add_filter( 'get_the_content_more_link', 'button_read_more_link' );
+	function button_read_more_link() {
+	return '<p><a class="button secondary" href="' . get_permalink() . '">Read more</a></p>';
+	}
+
+	// Line Awesome Shortcodes
+function addscFontAwesome($atts) {
+    extract(shortcode_atts(array(
+    'type'  => '',
+    'size' => '',
+    'rotate' => '',
+    'flip' => '',
+    'pull' => '',
+    'animated' => '',
+    'class' => '',
+
+    ), $atts));
+
+    $classes  = ($type) ? 'fa-'.$type. '' : 'fa-star';
+    $classes .= ($size) ? ' fa-'.$size.'' : '';
+    $classes .= ($rotate) ? ' fa-rotate-'.$rotate.'' : '';
+    $classes .= ($flip) ? ' fa-flip-'.$flip.'' : '';
+    $classes .= ($pull) ? ' pull-'.$pull.'' : '';
+    $classes .= ($animated) ? ' fa-spin' : '';
+    $classes .= ($class) ? ' '.$class : '';
+
+    $theAwesomeFont = '<i class="fa '.esc_html($classes).'"></i>';
+
+    return $theAwesomeFont;
+}
+
+add_shortcode('icon', 'addscFontAwesome');
+
+// Setup widget counts.
+function masa_count_widgets( $id ) {
+
+	$sidebars_widgets = wp_get_sidebars_widgets();
+
+	if ( isset( $sidebars_widgets[ $id ] ) ) {
+		return count( $sidebars_widgets[ $id ] );
 	}
 
 }
+
+// Calculate widget count.
+function masa_widget_area_class( $id ) {
+
+	$count = masa_count_widgets( $id );
+
+	$class = '';
+
+	if ( $count == 1 ) {
+		$class .= ' widget-full';
+	} elseif ( $count % 3 == 1 ) {
+		$class .= ' widget-thirds';
+	} elseif ( $count % 4 == 1 ) {
+		$class .= ' widget-fourths';
+	} elseif ( $count % 2 == 0 ) {
+		$class .= ' widget-halves uneven';
+	} else {
+		$class .= ' widget-halves';
+	}
+
+	return $class;
+
+}
+// Add single post navigation.
+add_action( 'genesis_entry_footer', 'genesis_prev_next_post_nav' );
+
+// Register shop sidebar widget area.
+genesis_register_sidebar( array(
+	'id'          => 'shop-sidebar',
+	'name'        => __( 'Shop Sidebar', 'genesis-sample' ),
+	'description' => __( 'This is the shop sidebar widget area if you are using a two column site layout option for your product archive.', 'genesis-sample' ),
+) );
+
+/**
+ * Display shop sidebar widget area.
+ */
+function sp_shop_widget_area() {
+
+	if ( class_exists( 'WooCommerce' ) && is_woocommerce() ) {
+
+		genesis_widget_area( 'shop-sidebar', array(
+		    'before' => '<div class="shop-sidebar">',
+		    'after'  => '</div>',
+		) );
+	}
+}
+add_action( 'genesis_before_sidebar_widget_area', 'sp_shop_widget_area' );
--- lib/customize.php
+++ lib/customize.php
@@ -7,46 +7,45 @@
  * @package Genesis Sample
  * @author  StudioPress
  * @license GPL-2.0+
- * @link    http://www.studiopress.com/
+ * @link    https://www.studiopress.com/
  */
-
-/**
- * Get default link color for Customizer.
- *
- * Abstracted here since at least two functions use it.
- *
- * @since 2.2.3
- *
- * @return string Hex color code for link color.
- */
-function genesis_sample_customizer_get_default_link_color() {
-	return '#c3251d';
-}
-
-/**
- * Get default accent color for Customizer.
- *
- * Abstracted here since at least two functions use it.
- *
- * @since 2.2.3
- *
- * @return string Hex color code for accent color.
- */
-function genesis_sample_customizer_get_default_accent_color() {
-	return '#c3251d';
-}
 
 add_action( 'customize_register', 'genesis_sample_customizer_register' );
 /**
- * Register settings and controls with the Customizer.
+ * Registers settings and controls with the Customizer.
  *
  * @since 2.2.3
- * 
+ *
  * @param WP_Customize_Manager $wp_customize Customizer object.
  */
-function genesis_sample_customizer_register() {
+function genesis_sample_customizer_register( $wp_customize ) {
 
-	global $wp_customize;
+	$images = apply_filters( 'monochrome_images', array( '1', '4' ) );
+
+	$wp_customize->add_section( 'monochrome-settings', array(
+		'description' => __( 'Use the included default images or personalize your site by uploading your own images.<br /><br />The default images are <strong>1600 pixels wide and 800 pixels tall</strong>.', 'monochrome-pro' ),
+		'title'       => __( 'Front Page Background Images', 'monochrome-pro' ),
+		'priority'    => 35,
+	) );
+
+	foreach( $images as $image ) {
+
+		// Add setting for front page background images.
+		$wp_customize->add_setting( $image .'-monochrome-image', array(
+			'default'           => sprintf( '%s/images/bg-%s.jpg', get_stylesheet_directory_uri(), $image ),
+			'sanitize_callback' => 'esc_url_raw',
+			'type'              => 'option',
+		) );
+
+		// Add control for front page background images.
+		$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, $image .'-monochrome-image', array(
+			'label'    => sprintf( __( 'Featured Section %s Image:', 'monochrome-pro' ), $image ),
+			'section'  => 'monochrome-settings',
+			'settings' => $image .'-monochrome-image',
+			'priority' => $image+1,
+		) ) );
+
+	}
 
 	$wp_customize->add_setting(
 		'genesis_sample_link_color',
@@ -61,10 +60,10 @@ function genesis_sample_customizer_register() {
 			$wp_customize,
 			'genesis_sample_link_color',
 			array(
-				'description' => __( 'Change the default color for linked titles, menu links, post info links and more.', 'genesis-sample' ),
-			    'label'       => __( 'Link Color', 'genesis-sample' ),
-			    'section'     => 'colors',
-			    'settings'    => 'genesis_sample_link_color',
+				'description' => __( 'Change the color of post info links, hover color of linked titles, hover color of menu items, and more.', 'genesis-sample' ),
+				'label'       => __( 'Link Color', 'genesis-sample' ),
+				'section'     => 'colors',
+				'settings'    => 'genesis_sample_link_color',
 			)
 		)
 	);
@@ -82,11 +81,36 @@ function genesis_sample_customizer_register() {
 			$wp_customize,
 			'genesis_sample_accent_color',
 			array(
-				'description' => __( 'Change the default color for button hovers.', 'genesis-sample' ),
-			    'label'       => __( 'Accent Color', 'genesis-sample' ),
-			    'section'     => 'colors',
-			    'settings'    => 'genesis_sample_accent_color',
+				'description' => __( 'Change the default hovers color for button.', 'genesis-sample' ),
+				'label'       => __( 'Accent Color', 'genesis-sample' ),
+				'section'     => 'colors',
+				'settings'    => 'genesis_sample_accent_color',
 			)
+		)
+	);
+
+	$wp_customize->add_setting(
+		'genesis_sample_logo_width',
+		array(
+			'default'           => 350,
+			'sanitize_callback' => 'absint',
+		)
+	);
+
+	// Add a control for the logo size.
+	$wp_customize->add_control(
+		'genesis_sample_logo_width',
+		array(
+			'label'       => __( 'Logo Width', 'genesis-sample' ),
+			'description' => __( 'The maximum width of the logo in pixels.', 'genesis-sample' ),
+			'priority'    => 9,
+			'section'     => 'title_tagline',
+			'settings'    => 'genesis_sample_logo_width',
+			'type'        => 'number',
+			'input_attrs' => array(
+				'min' => 100,
+			),
+
 		)
 	);
 
